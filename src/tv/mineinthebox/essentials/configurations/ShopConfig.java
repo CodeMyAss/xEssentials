31b3a58a96cee78ddd2ca52f80f7c984ae135f4d
xize <glucassen@live.nl>
2014-03-24 17:28:09 +0100

    did some minor changes

--- src/tv/mineinthebox/essentials/configurations/ShopConfig.java
+++ src/tv/mineinthebox/essentials/configurations/ShopConfig.java
@@ -13,22 +13,6 @@ public class ShopConfig {
 		return (String) Configuration.getConfigValue(ConfigType.SHOP, "AdminShopPrefix");
 	}
 	
-	public boolean isTaxEnabled() {
-		return (Boolean) Configuration.getConfigValue(ConfigType.SHOP, "isTaxEnabled");
-	}
-	
-	public Double getTax() {
-		return (Double) Configuration.getConfigValue(ConfigType.SHOP, "shop.tax.price");
-	}
-	
-	public boolean isCooldownEnabled() {
-		return (Boolean) Configuration.getConfigValue(ConfigType.SHOP, "isCooldown");
-	}
-	
-	public int getCooldown() {
-		return (Integer) Configuration.getConfigValue(ConfigType.SHOP, "getCooldownTime");
-	}
-	
 	public boolean isShopMessagesDisabled() {
 		return (Boolean) Configuration.getConfigValue(ConfigType.SHOP, "disableMessages");
 	}

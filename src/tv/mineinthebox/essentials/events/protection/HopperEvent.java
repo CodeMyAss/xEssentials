f265f84da6596383a34ba00f99f55761a98688dd
xize <glucassen@live.nl>
2014-04-14 20:13:10 +0200

    make sure the hopper gets blocked correctly.

--- src/tv/mineinthebox/essentials/events/protection/HopperEvent.java
+++ src/tv/mineinthebox/essentials/events/protection/HopperEvent.java
@@ -1,6 +1,9 @@
 package tv.mineinthebox.essentials.events.protection;
 
 import org.bukkit.block.BlockState;
+import org.bukkit.block.Chest;
+import org.bukkit.block.DoubleChest;
+import org.bukkit.block.Furnace;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryMoveItemEvent;
@@ -11,12 +14,12 @@ public class HopperEvent implements Listener {
 	
 	@EventHandler
 	public void cancelhopper(InventoryMoveItemEvent e) {
-		if(e.getSource().getHolder() instanceof BlockState) {
+		if(e.getSource().getHolder() instanceof Chest || e.getSource().getHolder() instanceof DoubleChest || e.getSource().getHolder() instanceof Furnace) {
 			BlockState state = (BlockState)e.getSource().getHolder();
 			if(xEssentials.getProtectionDatabase().isRegistered(state.getBlock())) {
 				e.setCancelled(true);
 			}
-		} else if(e.getDestination().getHolder() instanceof BlockState) {
+		} else if(e.getDestination().getHolder() instanceof Chest || e.getDestination().getHolder() instanceof DoubleChest || e.getDestination() instanceof Furnace) {
 			BlockState state = (BlockState)e.getDestination().getHolder();
 			if(xEssentials.getProtectionDatabase().isRegistered(state.getBlock())) {
 				e.setCancelled(true);

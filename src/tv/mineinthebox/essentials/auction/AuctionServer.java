fe2782231defadb2ae97a71007967ab47ce19cd0
xize <glucassen@live.nl>
2014-04-14 16:01:03 +0200

    made a fix for the auction server where the wrong thread is created.

--- src/tv/mineinthebox/essentials/auction/AuctionServer.java
+++ src/tv/mineinthebox/essentials/auction/AuctionServer.java
@@ -33,8 +33,8 @@ public class AuctionServer implements Runnable {
 	      HandlerList handlers = new HandlerList();
 	      handlers.setHandlers(new Handler[]{resource_handler, new AuctionContentHandler()});
 	      this.server.setHandler(handlers);
-	      Thread webserverThread = new Thread(this);
-	      webserverThread.start();
+	      this.web = new Thread(this);
+	      this.web.start();
 	}
 
 	/**

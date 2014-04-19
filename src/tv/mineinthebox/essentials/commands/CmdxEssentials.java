8627af7d54dfbe0d4fdc764cfa96838db4f06257
xize <glucassen@live.nl>
2014-04-19 20:49:57 +0200

    cleaned up Runnable internals for lwc import

--- src/tv/mineinthebox/essentials/commands/CmdxEssentials.java
+++ src/tv/mineinthebox/essentials/commands/CmdxEssentials.java
@@ -1,16 +1,7 @@
 package tv.mineinthebox.essentials.commands;
 
 import java.io.File;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.World;
-import org.bukkit.block.Block;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 
@@ -19,14 +10,10 @@ import tv.mineinthebox.essentials.Warnings;
 import tv.mineinthebox.essentials.xEssentials;
 import tv.mineinthebox.essentials.enums.LogType;
 import tv.mineinthebox.essentials.enums.PermissionKey;
+import tv.mineinthebox.essentials.utils.LWCRunnable;
 import tv.mineinthebox.essentials.utils.TPS;
 
-public class CmdxEssentials implements Runnable {
-
-	private CommandSender sender;
-	private Thread lwcthread;
-	private Long timestart;
-	private Long timeend;
+public class CmdxEssentials {
 
 	public boolean execute(CommandSender sender, Command cmd, String[] args) {
 		if(cmd.getName().equalsIgnoreCase("xEssentials")) {
@@ -81,10 +68,8 @@ public class CmdxEssentials implements Runnable {
 										xEssentials.getPlugin().log("setting up a new workers thread, against server crashing.", LogType.INFO);
 										sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&2[&3xEssentials&2]&f " + ChatColor.GRAY + "starting importing lwc database contents in our own database..."));
 										sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&2[&3xEssentials&2]&f " + ChatColor.GRAY + "setting up a new workers thread, against server crashing."));
-										timestart = System.currentTimeMillis();
-										this.sender = sender;
-										lwcthread = new Thread(this);
-										lwcthread.start();
+										Thread thread = new Thread(new LWCRunnable(sender));
+										thread.start();
 										xEssentials.getPlugin().log("thread has been activated, now waiting for response, this could take up to 30 minutes depending on the size of the database.", LogType.INFO);
 										sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&2[&3xEssentials&2]&f " + ChatColor.GRAY + "thread has been activated, now waiting for response, this could take up to 30 minutes depending on the size of the database."));
 									} else {
@@ -105,45 +90,4 @@ public class CmdxEssentials implements Runnable {
 		}
 		return false;
 	}
-
-	@Override
-	public void run() {
-		try {
-			Connection con = DriverManager.getConnection("jdbc:sqlite:plugins/LWC/lwc.db");
-			Statement state = con.createStatement();
-			String query = "SELECT * FROM lwc_protections";
-			ResultSet set = state.executeQuery(query);
-			while(set.next()) {
-				String owner = set.getString("owner");
-				int x = set.getInt("x");
-				int y = set.getInt("y");
-				int z = set.getInt("z");
-				World world = Bukkit.getWorld(set.getString("world"));
-				if(world instanceof World) {
-					Block block = world.getBlockAt(x, y, z);
-					xEssentials.getProtectionDatabase().register(owner, block);
-				}
-			}
-			state.close();
-			set.close();
-			con.close();
-			timeend = System.currentTimeMillis();
-			if(sender instanceof CommandSender) {
-				sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&2[&3xEssentials&2]&f " + ChatColor.GRAY + "lwc database has successfully been intergrated, into our xEssentials system."));
-				sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&2[&3xEssentials&2]&f " + ChatColor.GRAY + "thread took currently " + (timeend-timestart) + "ms."));
-			}
-			xEssentials.getPlugin().log("lwc database has successfully been intergrated, into our xEssentials system.", LogType.INFO);
-			xEssentials.getPlugin().log("thread took currently " + (timeend-timestart) + "ms.", LogType.INFO);
-			try {
-				//graceful shutdown of the thread.
-				Thread.sleep(100000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-	}
-
 }

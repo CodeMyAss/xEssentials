33078e806fbe9c1a2abd14ead22364dc823751f3
xize <glucassen@live.nl>
2014-04-20 16:23:54 +0200

    removed annoying message when breaking signs

--- src/tv/mineinthebox/essentials/events/shops/SignNormalShopOpenEvent.java
+++ src/tv/mineinthebox/essentials/events/shops/SignNormalShopOpenEvent.java
@@ -195,8 +195,6 @@ public class SignNormalShopOpenEvent implements Listener {
 							e.getPlayer().sendMessage(ChatColor.RED + "you are not allowed todo that!");
 						}
 					}
-				} else {
-					e.getPlayer().sendMessage(ChatColor.RED + "you are not allowed to do that!");
 				}
 			}
 		}
